c18e21141036c08add342c965112821f7ce2417d
joel <joelmani@gmail.com>
2024-03-26 21:18:57 -0700

    Created a custom usercontrol and added some more button styles

---
+++ JustAnotherExpenseTracker/Views/UserControls/PlaceholderTextBox.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JustAnotherExpenseTracker.Views.UserControls
+{
+    /// <summary>
+    /// Interaction logic for PlaceholderTextBox.xaml
+    /// </summary>
+    public partial class PlaceholderTextBox : UserControl
+    {
+        public PlaceholderTextBox()
+        {
+            InitializeComponent();
+        }
+
+        private string placeholder;
+
+        public string Placeholder
+        {
+            get { return placeholder; }
+            set 
+            { 
+                placeholder = value;
+                tbPlaceholder.Text = placeholder; // Bad practice to do this...Will probably need to change it
+            }
+        }
+
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            txtInput.Clear();
+            txtInput.Focus();
+        }
+
+        private void txtInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if(string.IsNullOrEmpty(txtInput.Text)) 
+            {
+                tbPlaceholder.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                tbPlaceholder.Visibility = Visibility.Hidden;
+            }    
+        }
+    }
+}
