634dfe3a68e406c15723ab59338bd2400f9978ec
joel <joelmani@gmail.com>
2024-04-03 17:09:49 -0700

    Setting up the ViewModels

---
+++ JustAnotherExpenseTracker/ViewModels/LoginViewModel.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace JustAnotherExpenseTracker.ViewModels
+{
+    public class LoginViewModel : ViewModelBase
+    {
+        //Fields
+        private string _userName;
+        private SecureString _password;
+        private string _errorMessage;
+        private bool _isViewVisible = true;
+
+
+        //Property
+        public string UserName
+        {
+            get
+            {
+                return _userName;
+            }
+            set
+            {
+                _userName = value;
+                OnPropertyChanged(nameof(UserName));
+            }
+        }
+        public SecureString Password
+        {
+            get
+            {
+                return _password;
+            }
+            set 
+            {
+                _password = value;
+                OnPropertyChanged(nameof(Password));
+            }
+        }
+        public string ErrorMessage
+        {
+            get 
+            {
+                return _errorMessage;
+            }
+            set
+            {
+                _errorMessage = value;
+                OnPropertyChanged(nameof(ErrorMessage));
+            }
+        }
+        public bool IsViewVisible
+        {
+            get 
+            {
+                return _isViewVisible;
+            }
+            set 
+            {
+                _isViewVisible = value;
+                OnPropertyChanged(nameof(IsViewVisible));
+            }
+        }
+
+        //-> Commands
+        public ICommand LoginCommand { get; }
+        public ICommand RecoverPasswordCommand { get; }
+        public ICommand ShowPasswordCommand { get; }
+        public ICommand RememberPasswordCommand { get; }
+
+
+        //Constructor
+        public LoginViewModel()
+        {
+            LoginCommand = new ViewModelCommand(ExecuteLoginCommand, CanExecuteLoginCommand); 
+        }
+
+        private bool CanExecuteLoginCommand(object obj)
+        {
+            bool vaildData;
+            if(string.IsNullOrWhiteSpace(UserName) || UserName.Length < 3 || Password == null || Password.Length < 3) 
+            {
+                vaildData = false;
+            }
+            else
+            {
+                vaildData = true;
+            }
+
+            return vaildData;
+        }
+
+        private void ExecuteLoginCommand(object obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
