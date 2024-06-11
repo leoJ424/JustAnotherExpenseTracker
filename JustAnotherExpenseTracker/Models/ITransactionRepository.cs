42090c4699fcddc4249a06cf1559dc46264f9e52
joel <joelmani@gmail.com>
2024-06-11 09:22:45 +0530

    Added Repositories for Transaction Data

---
+++ JustAnotherExpenseTracker/Models/ITransactionRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JustAnotherExpenseTracker.Models
+{
+    public interface ITransactionRepository
+    {
+        List<Guid> ReturnTransactionIDsBasedOnCardIDs(DateTime date1, DateTime date2, Guid CardId);
+        List<Guid> ReturnTransactionIDsBasedOnBankIDs(DateTime date1, DateTime date2, Guid BankId);
+        TransactionDetailsModel ReturnTransactionDetais(Guid TransactionId);
+        List<KeyValuePair<DateTime, decimal>> ReturnCardTransactionAmountsGroupByDate(DateTime date1, DateTime date2, Guid CardId);
+    }
+}
---
+++ JustAnotherExpenseTracker/Repositories/TransactionRepository.cs
@@ -0,0 +1,123 @@
+﻿using JustAnotherExpenseTracker.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace JustAnotherExpenseTracker.Repositories
+{
+    public class TransactionRepository : RepositoryBase, ITransactionRepository
+    {
+
+        public TransactionDetailsModel ReturnTransactionDetais(Guid TransactionId)
+        {
+            TransactionDetailsModel transactionDetails = new TransactionDetailsModel();
+
+            using (var connection = GetConnection())
+            {
+                connection.Open();
+                using (var command = new SqlCommand())
+                {
+                    command.Connection = connection;
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    command.CommandText = "sp_getTransactionDetailsByID";
+                    command.Parameters.Add("@TransactionID", System.Data.SqlDbType.UniqueIdentifier).Value = TransactionId;
+
+                    using (var reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            var guid = reader.GetGuid(0);
+                            transactionDetails.TransactionID = (Guid)reader[0];
+                            transactionDetails.CategoryID = (Guid)reader[2];
+                            transactionDetails.RecipientID = (Guid)reader[3];
+                            transactionDetails.BankID = (Guid)reader[4];
+                            transactionDetails.CardID = (Guid)reader[5];
+                            transactionDetails.PaymentMethod = reader[6].ToString();
+                            transactionDetails.TransactionType = reader[7].ToString();    
+                            transactionDetails.Amount = (decimal)reader[8];    
+                            transactionDetails.RewardPoints = (decimal)reader[9];    
+                            transactionDetails.TransactionDate = Convert.ToDateTime(reader[10]);    
+                            transactionDetails.GeneralComments = reader[11].ToString();    
+                        }
+                    }
+                }
+            }
+
+            return transactionDetails;
+        }
+
+        public List<Guid> ReturnTransactionIDsBasedOnBankIDs(DateTime date1, DateTime date2, Guid BankId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Guid> ReturnTransactionIDsBasedOnCardIDs(DateTime date1, DateTime date2, Guid CardId)
+        {
+            List<Guid> transactionIds = new List<Guid>();
+
+            using (var connection = GetConnection())
+            {
+                connection.Open();
+                using (var command = new SqlCommand())
+                {
+                    command.Connection = connection;
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    command.CommandText = "sp_getTransactionDetailsBetweenDatesBasedOnCardID";
+                    command.Parameters.Add("@Date1", System.Data.SqlDbType.Date).Value = date1;
+                    command.Parameters.Add("@Date2", System.Data.SqlDbType.Date).Value = date2;
+                    command.Parameters.Add("@CardID", System.Data.SqlDbType.UniqueIdentifier).Value = CardId;
+
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            var guid = reader.GetGuid(0);
+                            transactionIds.Add(guid);
+                        }
+                    }
+                }
+            }
+
+            return transactionIds;
+        }
+
+        public List<KeyValuePair<DateTime, decimal>> ReturnCardTransactionAmountsGroupByDate(DateTime date1, DateTime date2, Guid CardId)
+        {
+            var returnList = new List<KeyValuePair<DateTime, decimal>>();
+
+            using (var connection = GetConnection())
+            {
+                connection.Open();
+                using (var command = new SqlCommand())
+                {
+                    command.Connection = connection;
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    command.CommandText = "sp_getTransactionDetailsBetweenDatesBasedOnCardIDGroupByDate";
+                    command.Parameters.Add("@Date1", System.Data.SqlDbType.Date).Value = date1;
+                    command.Parameters.Add("@Date2", System.Data.SqlDbType.Date).Value = date2;
+                    command.Parameters.Add("@CardID", System.Data.SqlDbType.UniqueIdentifier).Value = CardId;
+
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            returnList.Add(new KeyValuePair<DateTime, decimal>(Convert.ToDateTime(reader[1]), Convert.ToDecimal(reader[0])));
+                        }
+                    }
+                }
+            }
+
+            return returnList;
+
+        }
+    }
+}
